4dca056521d23e2f18f7aa59eb0c5721558207dd
Rassul <rasul.tabulov@gmail.com>
2021-08-29 19:33:27 +0600

    add credits

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,48 @@
-<template>
-  <h1 class="text-2xl mb-12">About page</h1>
+<script setup>
+const links = [
+  {
+    href: 'https://github.com/vueuse/vueuse',
+    name: 'vue-use',
+  },
+  {
+    href: 'https://github.com/antfu/vite-plugin-components',
+    name: 'vite-plugin-components',
+  },
+  {
+    href: 'https://github.com/hannoeru/vite-plugin-pages',
+    name: 'vite-plugin-pages',
+  },
+  {
+    href: 'https://github.com/anncwb/vite-plugin-imagemin',
+    name: 'vite-plugin-imagemin',
+  },
+  {
+    href: 'https://github.com/anncwb/vite-plugin-svg-icons',
+    name: 'vite-plugin-svg-icons',
+  },
+  {
+    href: 'https://github.com/windicss/windicss',
+    name: 'windicss',
+  },
+];
+</script>
 
-  <p>highly opinionated template</p>
+<template>
+  <h1 class="text-3xl mb-8">credits</h1>
+
+  <ul>
+    <li v-for="link in links" :key="link.href" class="mb-4">
+      <a
+        class="
+          text-purple-600
+          transition-colors
+          dark:text-purple-400
+          hover:opacity-75
+        "
+        target="_blank"
+        :href="link.href"
+        >{{ link.name }}</a
+      >
+    </li>
+  </ul>
 </template>
