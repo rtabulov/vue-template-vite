774d7e133852ce7bd57a53fd1fb7474c22c6d51b
Rassul <rasul.tabulov@gmail.com>
2021-09-16 10:30:17 +0600

    improve linting

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,12 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+
   plugins: [
     vue(),
     Pages(),
